ddd22cc18bafa6deb030f5db26cae9ab075cdf52
logia1997 <98291262+logia1997@users.noreply.github.com>
2023-11-09 19:30:08 -0300

    Delete PROYECTO directory

--- PROYECTO/views/modulos/navegacion/UsuarioNav.php
+++
@@ -1,9 +0,0 @@
-<ul class="seis">
-
-	<li><a href="index.php?action=Usuario">Perfil</a></li>
-	<li><a href="index.php?action=agua_usuario">Agua</a></li>
-	<li><a href="index.php?action=clap_usuario">Clap</a></li>
-	<li><a href="index.php?action=gas_usuario">Gas</a></li>
-	<li class="bloqueadoTex">Foro<!--<a href="index.php?action=Master"></a>--></li>
-	<li><a href="index.php?action=salir">Salir</a></li>
-</ul> 
--- PROYECTO/views/modulos/navegacion/aguaNav.php
+++
@@ -1,8 +0,0 @@
-<ul>
-
-
-    <li><a href="index.php?action=Agua">Lista</a></li>
-	<li><a href="index.php?action=facturacion_agua">Facturacion agua</a></li>
-	<li><a href="index.php?action=salir">Salir</a></li>
-
-</ul> 
